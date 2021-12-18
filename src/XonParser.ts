670699b2f2f2ea0eb5e9cc0133deb9b2eecb497d
Oliver-makes-code <kookooloopdaking@gmail.com>
2021-12-17 20:32:17 -0600

    Convert XML to XON

--- src/XonParser.ts
+++ src/XonParser.ts
@@ -19,7 +19,31 @@ export default class XonParser {
     private parseXml(input: string): string {
         let xml = parseXml(input);
         if (!xml) return "";
-        console.log(xml.root);
-        return "";
+        let root = xml.root;
+        
+        return this.convertToXon(root);
+    }
+
+    private convertToXon(root: XMLNode, indent: string = ""): string {
+        let output = "";
+        if (!root.tag) return "";
+        output += root.tag + " ";
+        if (root.keys)
+            for (let key in root.keys)
+                output += key + "=\"" + root.keys[key] + "\" ";
+        if (root.sub) {
+            if (root.sub.length == 1 && typeof root.sub[0] == "string")
+                output += "\"" + root.sub[0] + "\"";
+            else {
+                output += "{\n";
+                for (let sub of root.sub) {
+                    if (typeof sub == "string") output += indent + "  \"" + sub + "\"\n";
+                    else output += this.convertToXon(sub, indent + "  ");
+                }
+                output += "\n"+indent+"}";
+            }
+        }
+        if (root.close) output = "\\" + output + "\n";
+        return indent + output;
     }
 }
